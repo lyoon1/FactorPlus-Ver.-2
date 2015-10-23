b4666ab71f1cf021b48ec6ddc7863d9f64d5ddad
Leo Yoon <LionKing@leos-macbook-air.local>
2015-10-23 10:07:41 -0400

    Initial Commit

---
+++ Factor+/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Factor+
+//
+//  Created by Leo Yoon on 2015-10-23.
+//  Copyright © 2015 LYM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
