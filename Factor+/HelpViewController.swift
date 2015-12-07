2e9181692ba28b12ec65fd430588edfc01f65030
Leo Yoon <LionKing@leos-macbook-air.local>
2015-12-07 15:48:44 -0500

    December 7th - 3:48 PM - Help screen started

--- Factor+/HelpViewController.swift
+++ Factor+/HelpViewController.swift
@@ -8,12 +8,33 @@
 
 import UIKit
 
-class HelpViewController: UIViewController {
+class HelpViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+    }
+    @IBOutlet weak var helpCollectionView: UICollectionView!
+    
+    
+    
+    let imageArray = [UIImage(named:"Help Screen 1"),UIImage(named:"Help Screen 2"),UIImage(named:"Help Screen 3"),UIImage(named:"Help Screen 4")]
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return imageArray.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("Help Cell", forIndexPath: indexPath) as! HelpCollectionViewCell
+        
+        cell.helpImage?.image = imageArray[indexPath.item]
+        
+        return cell
     }
 
     override func didReceiveMemoryWarning() {
