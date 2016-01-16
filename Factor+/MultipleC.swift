a4747282f5ac90f0bb3e913c534b1771a0b43c2e
FactorAlpha <jma4@ocdsb.ca>
2016-01-16 17:01:18 -0500

    Update MultipleC.swift

--- Factor+/MultipleC.swift
+++ Factor+/MultipleC.swift
@@ -5,28 +5,31 @@
 //  Created by Jia Long Ma on 2015-11-06.
 //  Copyright © 2015 LYM. All rights reserved.
 //
+//  This class is designated for factoring multiple choice
+//  It takes randomly generated integers and returns them in an appropriate string so they can be used to create factors
+//  in the form of (kx + m)(jx + n)
 
 import Foundation
 import UIKit
 
-class multipleChoice {
+class multipleChoice { //Start of multipleChoice class
     
-    var r = Int()
-    var s = Int()
+    var r = Int() //will represent the "m" values in (kx + m)(jx + n)
+    var s = Int() //will represent the "n" values in (kx + m)(jx + n)
 
-    func getRAns(rI: Int) -> String {
+    func getRAns(rI: Int) -> String { //An integer "rI" is randomly generated and sent from another class
         
-        if(rI > 0) {
+        if(rI > 0) { //If rI is greater than 0, then the integer will be casted as a string and returned as "+ rI"
             
             return "+" + String(rI)
             
         }
-        else if(rI < 0) {
+        else if(rI < 0) { //If rI is less than 0, then the integer will be casted as a string and returned as "- rI"
             
             return String(rI)
             
         }
-        else {
+        else { //If rI is 0, then nothing will be returned
             
             return ""
             
@@ -34,7 +37,7 @@ class multipleChoice {
         
     }
     
-    func getSAns(sI: Int) -> String {
+    func getSAns(sI: Int) -> String { //An integer "sI" is randomly generated and sent from another class
         
         if(sI > 0) {
             
@@ -53,7 +56,7 @@ class multipleChoice {
         }
     }
     
-    func getKAns(kI: Int) -> String {
+    func getKAns(kI: Int) -> String { //An integer "kI" is randomly generated and sent from another class
         
         if(kI == 1) {
             
@@ -67,7 +70,7 @@ class multipleChoice {
         
     }
     
-    func getJAns(jI: Int) -> String {
+    func getJAns(jI: Int) -> String { //An integer "jI" is randomly generated and sent from another class
         
         if(jI == 1) {
             
